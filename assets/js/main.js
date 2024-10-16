9d3332ef5bd45efb0cedcbf1be9d8c04f2e823dc
Kiriagoray <149868054+Kiriagoray@users.noreply.github.com>
2024-10-16 18:57:34 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+
+//get the button element
+
+const javaclass=document.getElementById('ColorButton)
+
+//add event listner for the botton click
+
+javaclass.addEventListener('click',addEventListener:():void=>{
+
+   javaclas.style.background=javaclass.style.backgroundColor==='red' ? '#4CAF50' :'red'
+
+});
+
+
+
+
+
+
